9a87d927e700e30ef942f1fb36a5f5ae0a5833a7
Peter Varga <varga.peter@binvision.hu>
2023-10-08 23:07:11 +0200

    chore: describe core logic of 2048 with comments

--- src/lib/game2048.js
+++ src/lib/game2048.js
@@ -1,24 +1,66 @@
+import sampleSize from 'lodash/sampleSize'
+import {
+	createEmptyMatrix,
+} from './utils'
+
 const Game2048 = (boardSize, renderCb) => {
-	let matrix = null
+	let matrix = createEmptyMatrix(boardSize)
+	let status = 'playing'
 
-	const init = () => {
-		console.log("init", boardSize)
-		matrix = Array(boardSize).fill(Array(boardSize).fill(0))
-		render()
-	}
-
-	const moveTiles = (direction) => {
-		console.log('move', direction)
-	}
-
+	// call external rendering
 	const render = () => {
-		if (renderCb) renderCb(matrix)
+		if (renderCb) renderCb({
+			matrix,
+			status
+		})
 	}
 
-	init()
 
+	// machine can add 2s
+	const moveMachine = () => {
+		// check if any zero cells left
+
+		// add 2s to zero cells
+
+		render()
+		// next: wait for user 
+	}
+
+	// user can move the board 
+	const moveUser = (direction) => {
+
+		// rotate matrix
+
+		// merge columns 
+
+		// rotate it back 
+
+		// render
+		render()
+
+		// next: machine
+		moveMachine()
+	}
+
+
+	// game lost
+	const isLost = () => {
+		// no more cells left to merge
+		// no more zeros left in the matrix 
+	}
+
+	// game won
+	const isWon = () => {
+		// at least one item === 2048 
+	}
+
+
+	// start the game and wait for the user
+	moveMachine()
+
+	// expose move to ui 
 	return {
-		moveTiles,
+		moveUser
 	}
 }
 
